d8d5f9ba94953b712a0e0c7c8728a8f16474edcb
jaskaa74 <jaska.sidhu499@gmail.com>
2025-03-15 11:48:36 +0100

    translation in java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,67 @@
 package org.example;
+import java.util.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void waitForSendKey() {
+        System.out.println("\nPremi Invio per tornare al menu principale.");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        bankClient firstClient = new bankClient("Giovanni");
+        int choice = 0;
+        List<Double> investimentsV = new ArrayList<>();
+
+        while (choice != 7) {
+            System.out.println("\nCosa vuoi fare?:");
+            System.out.println("1 - Deposito");
+            System.out.println("2 - Prelievo");
+            System.out.println("3 - Investimento");
+            System.out.println("4 - Avanza nel tempo");
+            System.out.println("5 - Visualizza lo stato del tuo conto");
+            System.out.println("6 - Visualizza lo stato del tuo portafoglio");
+            System.out.println("7 - Chiudi applicazione\n");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Inserisci la somma da depositare");
+                    System.out.println("ATTENZIONE: SE LA CIFRA È SUPERIORE A QUELLA DEL PORTAFOGLIO LA DIFFERENZA NON VERRÀ DEPOSITATA");
+                    System.out.println("SE IL CONTO È IN NEGATIVO VERRÀ UTILIZZATA PER RISANARE IL DEBITO");
+                    double depositSum = scanner.nextDouble();
+                    firstClient.deposit(depositSum);
+                    break;
+                case 2:
+                    System.out.println("Inserisci la somma da prelevare");
+                    System.out.println("ATTENZIONE: SE IL CONTO È IN NEGATIVO NON SARÀ POSSIBILE PRELEVARE");
+                    double takeSum = scanner.nextDouble();
+                    firstClient.takeMoney(takeSum);
+                    break;
+                case 3:
+                    investimentsV = firstClient.invest();
+                    break;
+                case 4:
+                    System.out.println("Di quanti mesi vuoi avanzare?");
+                    int JumpMonths = scanner.nextInt();
+                    firstClient.JumpMoths(investimentsV, JumpMonths);
+                    break;
+                case 5:
+                    firstClient.printStatus();
+                    break;
+                case 6:
+                    firstClient.printWallet();
+                    break;
+                case 7:
+                    scanner.close();
+                    return;
+                default:
+                    break;
+            }
+        }
+        scanner.close();
     }
 }
---
+++ src/main/java/org/example/bankClient.java
@@ -0,0 +1,186 @@
+package org.example;
+import java.util.*;
+
+public class bankClient {
+    private String name;
+    private double balance;
+    private double wallet;
+
+    public bankClient(String name, double balance, double wallet) {
+        this.name = name;
+        this.balance = balance;
+        this.wallet = wallet;
+    }
+
+    public bankClient(String name) {
+        this(name, 0, 100);
+    }
+
+
+    public void printBalance() {
+        System.out.println(balance);
+    }
+
+    public void printWallet() {
+        System.out.println(wallet);
+    }
+
+    public void printStatus() {
+        System.out.println("Stato utente:\nNome utente: " + name + "\nConto: " + balance);
+    }
+
+    public void deposit(double moneyToDeposit) {
+        if (balance < 0) {
+            balance += moneyToDeposit;
+        }
+        if (wallet < moneyToDeposit) {
+            System.out.println("Solo " + wallet + " euro sono stati depositati");
+            moneyToDeposit = wallet;
+            wallet = 0;
+        } else {
+            wallet -= moneyToDeposit;
+        }
+        balance += moneyToDeposit;
+    }
+
+    public void takeMoney(double moneyToTake) {
+        if (balance <= 0) {
+            System.out.println("Conto vuoto");
+            return;
+        }
+        if (moneyToTake > balance) {
+            System.out.println("Soldi prelevati: " + balance + " euro");
+            moneyToTake = balance;
+            balance = 0;
+        }
+        wallet += moneyToTake;
+        balance -= moneyToTake;
+    }
+
+    public List<Double> shortInvestiment(List<Double> finalResults) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Scelta:\n1-Investimento di basso rischio e basso guadagno\n2-Investimento di medio rischio e medio guadagno\n3-Investimento di alto rischio e alto guadagno");
+        int choice = scanner.nextInt();
+
+        double[] winnings = {0.7, 0.8, 0.9, 1.0, 1.1, 1.1, 1.1, 1.1, 1.2, 1.2};
+        Random rand = new Random();
+        double moneyToInvest;
+
+        System.out.print("soldi disponibili: ");
+        printBalance();
+        System.out.println("Quanto vuoi investire?");
+        moneyToInvest = scanner.nextDouble();
+
+        if (moneyToInvest > balance) {
+            System.out.println((moneyToInvest - balance) + " euro non sono stati investiti");
+            moneyToInvest = balance;
+            balance = 0;
+        } else {
+            balance -= moneyToInvest;
+        }
+
+        for (int duration = 0; duration < 12; duration++) {
+            int controlInvestiment = rand.nextInt(10);
+            moneyToInvest *= winnings[controlInvestiment];
+            finalResults.add(moneyToInvest);
+        }
+        return finalResults;
+    }
+
+    public List<Double> mediumInvestiment(List<Double> finalResults) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Investimento a medio termine (4 anni)");
+
+        double[] winnings = {0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.2, 1.3, 1.3, 1.5};
+        Random rand = new Random();
+        double moneyToInvest;
+
+        System.out.print("Soldi disponibili: ");
+        printBalance();
+        System.out.println("Quanto vuoi investire?");
+        moneyToInvest = scanner.nextDouble();
+
+        if (moneyToInvest > balance) {
+            System.out.println((moneyToInvest - balance) + " euro non sono stati investiti");
+            moneyToInvest = balance;
+            balance = 0;
+        } else {
+            balance -= moneyToInvest;
+        }
+
+        for (int duration = 0; duration < 4; duration++) { // 4 anni
+            int controlInvestiment = rand.nextInt(10);
+            moneyToInvest *= winnings[controlInvestiment];
+            finalResults.add(moneyToInvest);
+        }
+        return finalResults;
+    }
+
+    public List<Double> longInvestiment(List<Double> finalResults) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Investimento a lungo termine (10 anni)");
+
+        double[] winnings = {0.5, 0.6, 0.7, 0.8, 1.0, 1.2, 1.5, 1.7, 2.0, 2.5};
+        Random rand = new Random();
+        double moneyToInvest;
+
+        System.out.print("Soldi disponibili: ");
+        printBalance();
+        System.out.println("Quanto vuoi investire?");
+        moneyToInvest = scanner.nextDouble();
+
+        if (moneyToInvest > balance) {
+            System.out.println((moneyToInvest - balance) + " euro non sono stati investiti");
+            moneyToInvest = balance;
+            balance = 0;
+        } else {
+            balance -= moneyToInvest;
+        }
+
+        for (int duration = 0; duration < 10; duration++) { // 10 anni
+            int controlInvestiment = rand.nextInt(10);
+            moneyToInvest *= winnings[controlInvestiment];
+            finalResults.add(moneyToInvest);
+        }
+        return finalResults;
+    }
+
+
+    public List<Double> invest() {
+        Scanner scanner = new Scanner(System.in);
+        List<Double> finalResults = new ArrayList<>();
+        System.out.println("1 - Investimento breve durata (12 mesi)\n2 - Investimento media durata (4 anni)\n3 - Investimento lunga durata (10 anni)");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                shortInvestiment(finalResults);
+                break;
+            case 2:
+                mediumInvestiment(finalResults);
+                break;
+            case 3:
+                longInvestiment(finalResults);
+                break;
+            default:
+                System.out.println("Scelta inesistente");
+                invest();
+                break;
+        }
+        return finalResults;
+    }
+
+    public void JumpMoths(List<Double> investimentsV, int JumpM) {
+        wallet += JumpM * 100;
+        if (investimentsV.isEmpty()) {
+            printStatus();
+            return;
+        }
+        if (JumpM > investimentsV.size()) {
+            balance += investimentsV.get(investimentsV.size() - 1);
+        } else {
+            balance += investimentsV.get(JumpM - 1);
+        }
+        printStatus();
+    }
+}
